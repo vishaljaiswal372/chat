3443ddd8261a6d9a52e330538bf2099759177237
Vishal Jaiswal <kumardenzel@gmail.com>
2025-05-29 18:14:25 +0530

    home page completed
    home page the app is comleted

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,42 +8,65 @@ import CallPage from "./pages/CallPage.jsx"
 import OnboardingPage from "./pages/OnboardingPage.jsx";
 import NotificationPage from "./pages/NotificationPage.jsx";
 import toast,{Toaster} from "react-hot-toast";
-import { useQuery } from '@tanstack/react-query';
-import axios from "axios";
-import { axiosInstance } from './lib/axios.js';
+import PageLoader from './components/PageLoader.jsx';
+import useAuthUser from './hooks/useAuthUser.js';
+import Layout from "./components/Layout.jsx"
+import { useThemeStore } from './store/useThemeStore.js';
 
 const App = () => {
 
-  const {data:authData,idLoading,error}=useQuery({
-    queryKey:["authUser"],
-    queryFn:async ()=>{
-      const res=await axiosInstance.get("/auth/me");
-      return res.data;
-    },
-    retry:false, //auth check
-  });
+  const {isLoading,authUser}=useAuthUser();
 
-  const authUser=authData?.user;
+  const {theme}=useThemeStore();
 
-  if (idLoading) {
-    return (
-      <div className="h-screen flex items-center justify-center text-xl">
-        Checking authentication...
-      </div>
-    );
-  }
+  const isAuthenticated=Boolean(authUser);
+  const isOnboarded=authUser?.isOnboarded;
+
+  if (isLoading) return <PageLoader/>;
 
   return ( 
-    <div  className="h-screen" data-theme="coffee">
+    <div  className="h-screen" data-theme={theme}>
       {/* <button onClick={()=>toast.success("hello world!")} className='btn'>create a toast</button> */}
       <Routes>
-        <Route path="/" element={authUser ? <HomePage/>:<Navigate to="/login" />}/>
-        <Route path="/signup" element={!authUser ? <SignUpPage/>:<Navigate to="/" />}/>
-        <Route path="/login" element={!authUser ? <LoginPage/>:<Navigate to="/" />}/>
-        <Route path="/notification" element={authUser ? <NotificationPage/>:<Navigate to="/login" />}/>
-        <Route path="/chat" element={authUser ? <ChatPage/>:<Navigate to="/login" />}/>
-        <Route path="/call" element={authUser ? <CallPage/>:<Navigate to="/login" />}/>
-        <Route path="/onboarding" element={authUser ? <OnboardingPage/>:<Navigate to="/login" />}/>
+        <Route path="/" element={isAuthenticated && isOnboarded ? (
+          <Layout showSidebar={true}>
+            <HomePage/>
+          </Layout>
+        ):(
+          <Navigate to={!isAuthenticated ? "/login":"/onboarding"}/> 
+        )}
+        />
+        
+        <Route
+          path="/signup"
+          element={
+            !isAuthenticated ? <SignUpPage /> : <Navigate to={isOnboarded ? "/" : "/onboarding"} />
+          }
+        />
+        
+        <Route
+          path="/login"
+          element={
+            !isAuthenticated ? <LoginPage /> : <Navigate to={isOnboarded ? "/" : "/onboarding"} />
+          }
+        />
+        <Route path="/notifications" element={isAuthenticated ? <NotificationPage/>:<Navigate to="/login" />}/>
+        <Route path="/chat" element={isAuthenticated ? <ChatPage/>:<Navigate to="/login" />}/>
+        <Route path="/call" element={isAuthenticated ? <CallPage/>:<Navigate to="/login" />}/>
+        <Route
+          path="/onboarding"
+          element={
+            isAuthenticated ? (
+              !isOnboarded ? (
+                <OnboardingPage />
+              ) : (
+                <Navigate to="/" />
+              )
+            ) : (
+              <Navigate to="/login" />
+            )
+          }
+        />
       </Routes>
       <Toaster/>
     </div>
--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,6 +1,53 @@
+import axios from "axios";
 import { axiosInstance } from "./axios";
 
 export const signup=async(signupData)=>{
     const response=await axiosInstance.post("/auth/signup",signupData);
     return response.data;
+};
+
+export const login=async(loginData)=>{
+    const response=await axiosInstance.post("/auth/login",loginData);
+    return response.data;
+};
+
+export const logout = async () => {
+  const response = await axiosInstance.post("/auth/logout");
+  return response.data;
+};
+
+export const getAuthUser = async () => {
+  try {
+    const res = await axiosInstance.get("/auth/me");
+    return res.data;
+  } catch (error) {
+    console.log("Error in getAuthUser:", error);
+    return null;
+  }
+};
+
+export const completeOnboarding=async(userData)=>{
+    const response=await axiosInstance.post("/auth/onboarding",userData)
+    return response.data;
+}
+
+export const getUserFriends=async()=>{
+  const response=await axiosInstance.get("/users/friends");
+  return response.data;
+}
+
+export const getRecommendedUsers=async()=>{
+  const response=await axiosInstance.get("/users");
+  console.log("API Response:", response.data);
+  return response.data;
+}
+
+export const getOutgoingFriendReqs=async()=>{
+  const response=await axiosInstance.get("/users/outgoing-friend-requests");
+  return response.data;
+}
+
+export const sendFriendRequest=async(userId)=>{
+  const response=await axiosInstance.post(`/users/friend-request/${userId}`);
+  return response.data;
 }
